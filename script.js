9bda3d529ccad077fff048313b55e2ae1e0431f6
flug8 <80972789+flug8@users.noreply.github.com>
2023-08-20 16:38:39 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let nn = new NeuralNetwork(2, 3, 1, 0.1);
+
+let inputs = [  [0, 0], [0, 1], [1, 0], [1, 1] ];
+let results = [ [0], [1], [1], [0] ];
+
+let canvas = document.getElementById("testCanvas");
+let ctx = canvas.getContext("2d");
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+function visualTest() {
+    for (let i = 0; i < 100; i++) {
+        let index = Math.floor(Math.random() * 4);
+        nn.bp(inputs[index], results[index]);
+        let a = Math.floor(Math.random()*50)/50;
+        let b = Math.floor(Math.random()*50)/50;
+        let c = nn.ff([a,b]);
+        ctx.fillStyle = `rgb(${c[0]*255}, ${c[0]*255}, ${c[0]*255})`;
+        ctx.fillRect(a*canvas.width, b*canvas.height, canvas.width/50, canvas.height/50);
+    }
+    window.requestAnimationFrame(visualTest);
+}
